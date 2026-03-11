387e80955d42accd691d42dd9de5fd917f4056c9
Jeffrey Gates <jeffreymrgates@gmail.com>
2026-03-11 13:30:12 -0400

    Finished ChallengeSet02, wrote notes for myself!

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,63 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            return numbers == null || numbers.Count() == 0 ? 0 : numbers.Min() +numbers.Max();
+            //The instructor was having trouble getting the computer remember the numbers
+            // while doing this the long way, remember to ask how these Min and Max methods are keeping track of the numbers
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length < str2.Length ? str1.Length : str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers == null || numbers.Length == 0 ? 0 : numbers.Sum();
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers == null || numbers.Length == 0 ? 0 : numbers.Where(x => x % 2 == 0).Sum();
+            //This might be a good example to explain yourself what's going on here:
+            // Numbers == null is checking to see if it's a null, OR if the length equals 0, and then what to do if
+            // it's not zero
+            
+            //The Where method here is taking a statement (internet says it's called a predicate)
+            //"x" here is whatever number is put in, separated by what's called a "Lambda Operator" (=>)
+            // "=>" acts as a separates the input and the expression
+            //So it's checking if the remainder of the number is 0, and then using the built in Sum function if it is
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            return numbers != null && numbers.Count != 0 && numbers.Sum() % 2 != 0;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            return number <= 0 ? 0 : number / 2;
+            //Remember: you're dividing this by two because odds are every other number!
         }
     }
 }
